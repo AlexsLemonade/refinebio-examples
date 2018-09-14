5a800e5acfbf6118ef54f32b93bd1d37b3765bfe
cansav09 <savonenc@msu.edu>
2018-09-14 15:12:38 -0400

    gct_file script more usable
    Incorporated Jackie's comments
    Made the script so it can be used from command line

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # refinebio-examples
+
+### This repository contains example workflows of how to use data downloaded from <a href="refine.bio.org"> refine.bio </a>
+
+# Table of Contents
+
+Example workflows using GenePattern 
+    Converting files to GenePattern compatible formats
+
+Example workflows in R 
+    Clustering data 
+    
+# Example workflows using GenePattern 
+## Converting files to GenePattern compatible formats
+<a href="http://genepattern-notebook.org/example-notebooks/"> GenePattern notebooks </a> have numerous ready-made analyses. 
+
+### Converting files to gct format
+In order to create a gct formatted file from a tsv refin.bio data file, reference the create_gct_file.R script, followed by the name of the file in your current directory that you would like to convert.  
+
+'$ Rscript create_gct_file.R GSE111111.tsv'
+
+Optionally you can designate the name of the output file by adding an argument following the name of the input file. The "gct" suffix will be added if you do not add it yourself.  Be sure to either have the script and input file in your current working directory, or put type out the full directory path for the script and/or input file. eg "users/Bob/Desktop/create_gct_file.R"
+
+'$ Rscript create_gct_file.R GSE111111.tsv outputname'
+
+
+# Example workflows in R
+Clustering data 
+
+
+
--- create_gct_file.R
+++ create_gct_file.R
@@ -1,36 +1,44 @@
-# C. Savonen 2018 
+# C. Savonen 2018
 
-# Objective: Convert a "Tab Separated Values" file into a "Gene Cluster Text" file, for use in GenePattern Notebooks.
+# Convert a gene expression tab separated values (tsv) file provided into a gene cluster text (gct) file for use in GenePattern notebooks.
 
-# Read in whatever tsv file is in the directory
-file.name <- grep(".tsv", dir(), value=TRUE)
+args <- commandArgs(trailingOnly=TRUE)
+
+# Only continue with execution if filename is given
+if (length(args) == 0) {
+  stop("Input file name needed", call. = FALSE)
+} else if (length(args) == 1) {
+  # If no output file name is specified, use input filename
+  args[2] <- gsub("\\.tsv$", ".gct", args[1])
+}
+
+# Check if file is a tsv file
+if(grepl("\\.tsv$", args[1]) == FALSE){
+    stop("Only tsv files can be used as input files", call. = FALSE)
+}
+
+# Assign input file name
+file.name <- args[1]
+
+# Assign output file name, and if it doesn't include a ".gct" suffix, then add a ".gct" suffix.
+output.file <- ifelse(grepl("\\.gct$", args[2]), args[2], paste0(args[2], ".gct"))
 
 # Read in the file
 df <- read.table(file.name, stringsAsFactors = FALSE)
 
-# Format columns to GCT column names. Create a "Description" dummy variable column.
-df <- data.frame("Name" = rownames(df), "Description" = rep(NA, nrow(dat)), dat)
-
-# Get rid of ".CEL" prefix if it exists.
-colnames(df) <- gsub("\\.CEL", "" , colnames(df))
-
-# Get rid of old ".tsv" suffix and replace with "gct"
-file.name <- gsub("tsv", "gct", file.name)
+# Format columns to gct column names. Create a "Description" dummy variable column.
+df <- data.frame("Name" = rownames(df), "Description" = rep(NA, nrow(df)), df)
 
 # Get the number of genes and samples.
 num.samples <- ncol(df) - 2
 num.genes <- nrow(df)
 
 # Make .gct specific Header with the number of genes and samples in the second line. 
-header <- c("#1.2", paste0( c(num.genes, num.samples), collapse="\t"))
+header <- c("#1.2", paste0(c(num.genes, num.samples), collapse = "\t"))
 
 # Write the header and then append the data to the header
-write(header, file = file.name)
-write.table(df, row.names = FALSE, file.name,append = TRUE, quote = FALSE, sep="\t")
-
-# It will warn us that we are appending column names to the file, but that's what we want to do since we need that header at the top for GenePattern to recognize it. 
-
-sessionInfo()
-
-
+write(header, file = output.file)
+suppressWarnings(write.table(df, row.names = FALSE, output.file, append = TRUE, quote = FALSE, sep="\t"))
+        
+# It will warn us that we are appending column names to the file, but that's what we want to do since we need that header at the top for GenePattern to recognize it.
 
