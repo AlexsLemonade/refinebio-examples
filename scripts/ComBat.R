fa9fcf91b2af8efe8c709efcd3c391e63a8175f4
Candace Savonen <savonenc@msu.edu>
2019-04-05 13:40:02 -0400

    Have good ComBat example

--- scripts/ComBat.R
+++ scripts/ComBat.R
@@ -1,16 +1,16 @@
-# 'expression_xls' is the expression index file (e.g. outputted by dChip); 'sample_info_file' is a tab-delimited text file containing the colums: Array  name, sample name, Batch, and any other covariates to be included in the modeling; 'type' currently supports two data file types 'txt' for a tab-delimited text file and 'csv' for an Excel .csv file (sometimes R handles the .csv file better, so use this if you have problems with a .txt file!); 'write' if 'T' ComBat writes adjusted data to a file, and if 'F' and ComBat outputs the adjusted data matrix if 'F' (so assign it to an object! i.e. NewData <- ComBat('my expression.xls','Sample info file.txt', write=F)); 'covariates=all' will use all of the columns in your sample info file in the modeling (except array/sample name), if you only want use a some of the columns in your sample info file, specify these columns here as a vector (you must include the Batch column in this list); 'par.prior' if 'T' uses the parametric adjustments, if 'F' uses the nonparametric adjustments--if you are unsure what to use, try the parametric adjustments (they run faster) and check the plots to see if these priors are reasonable; 'filter=value' filters the genes with absent calls in > 1-value of the samples. The defaut here (as well as in dchip) is .8. Filter if you can as the EB adjustments work better after filtering. Filter must be numeric if your expression index file contains presence/absence calls (but you can set it >1 if you don't want to filter any genes) and must be 'F' if your data doesn't have presence/absence calls; 'skip' is the number of columns that contain probe names and gene information, so 'skip=5' implies the first expression values are in column 6; 'prior.plots' if true will give prior plots with black as a kernal estimate of the empirical batch effect density and red as the parametric estimate. 
+# 'expression_xls' is the expression index file (e.g. outputted by dChip); 'sample_info_file' is a tab-delimited text file containing the colums: Array  name, sample name, Batch, and any other covariates to be included in the modeling; 'type' currently supports two data file types 'txt' for a tab-delimited text file and 'csv' for an Excel .csv file (sometimes R handles the .csv file better, so use this if you have problems with a .txt file!); 'write' if 'T' ComBat writes adjusted data to a file, and if 'F' and ComBat outputs the adjusted data matrix if 'F' (so assign it to an object! i.e. NewData <- ComBat('my expression.xls','Sample info file.txt', write=F)); 'covariates=all' will use all of the columns in your sample info file in the modeling (except array/sample name), if you only want use a some of the columns in your sample info file, specify these columns here as a vector (you must include the Batch column in this list); 'par.prior' if 'T' uses the parametric adjustments, if 'F' uses the nonparametric adjustments--if you are unsure what to use, try the parametric adjustments (they run faster) and check the plots to see if these priors are reasonable; 'filter=value' filters the genes with absent calls in > 1-value of the samples. The defaut here (as well as in dchip) is .8. Filter if you can as the EB adjustments work better after filtering. Filter must be numeric if your expression index file contains presence/absence calls (but you can set it >1 if you don't want to filter any genes) and must be 'F' if your data doesn't have presence/absence calls; 'skip' is the number of columns that contain probe names and gene information, so 'skip=5' implies the first expression values are in column 6; 'prior.plots' if true will give prior plots with black as a kernal estimate of the empirical batch effect density and red as the parametric estimate.
 
 ComBat <- function(expression_xls, sample_info_file, type='txt', write=T, covariates='all', par.prior=T, filter=F, skip=0, prior.plots=T){
 	#debug: expression_xls='exp.txt'; sample_info_file='sam.txt'; type='txt'; write=T; covariates='all'; par.prior=T; filter=F; skip=0; prior.plots=T
 	cat('Reading Sample Information File\n')
 	saminfo <- read.table(sample_info_file, header=T, sep='\t',comment.char='')
 	if(sum(colnames(saminfo)=="Batch")!=1){return('ERROR: Sample Information File does not have a Batch column!')}
-		
+
 	cat('Reading Expression Data File\n')
 	if(type=='csv'){
 		dat <- read.csv(expression_xls,header=T,as.is=T)
                 #print(dat[1:2,])
-	#	dat <- dat[,trim.dat(dat)]  
+	#	dat <- dat[,trim.dat(dat)]
                 #print(colnames(dat))
 		colnames(dat)=scan(expression_xls,what='character',nlines=1,sep=',',quiet=T)[1:ncol(dat)]
                 #print(colnames(dat))
@@ -26,7 +26,7 @@ ComBat <- function(expression_xls, sample_info_file, type='txt', write=T, covari
         else{geneinfo=NULL}
         #print(geneinfo[1:4])
         #print(dat[1:2,])
-	
+
 	if(filter){
 		ngenes <- nrow(dat)
 		col <- ncol(dat)/2
@@ -37,22 +37,22 @@ ComBat <- function(expression_xls, sample_info_file, type='txt', write=T, covari
 		}
 
 	if(any(apply(dat,2,mode)!='numeric')){return('ERROR: Array expression columns contain non-numeric values! (Check your .xls file for non-numeric values and if this is not the problem, make a .csv file and use the type=csv option)')}
-	
+
 	tmp <- match(colnames(dat),saminfo[,1])
 	if(any(is.na(tmp))){return('ERROR: Sample Information File and Data Array Names are not the same!')}
 	#tmp1 <- match(saminfo[,1],colnames(dat))
 	#saminfo <- saminfo[tmp1[!is.na(tmp1)],]
-	saminfo <- saminfo[tmp,]  ## Bug fixed 01/04/2011		
+	saminfo <- saminfo[tmp,]  ## Bug fixed 01/04/2011
 
 	if(any(covariates != 'all')){saminfo <- saminfo[,c(1:2,covariates)]}
-	design <- design.mat(saminfo)	
+	design <- design.mat(saminfo)
 
 
 	batches <- list.batch(saminfo)
 	n.batch <- length(batches)
 	n.batches <- sapply(batches, length)
 	n.array <- sum(n.batches)
-	
+
 	## Check for missing values
 	NAs = any(is.na(dat))
 	if(NAs){cat(c('Found',sum(is.na(dat)),'Missing Data Values\n'),sep=' ')}
@@ -61,10 +61,10 @@ ComBat <- function(expression_xls, sample_info_file, type='txt', write=T, covari
 	cat('Standardizing Data across genes\n')
 	if (!NAs){B.hat <- solve(t(design)%*%design)%*%t(design)%*%t(as.matrix(dat))}else{B.hat=apply(dat,1,Beta.NA,design)} #Standarization Model
 	grand.mean <- t(n.batches/n.array)%*%B.hat[1:n.batch,]
-	if (!NAs){var.pooled <- ((dat-t(design%*%B.hat))^2)%*%rep(1/n.array,n.array)}else{var.pooled <- apply(dat-t(design%*%B.hat),1,var,na.rm=T)}
+	if (!NAs){var.pooled <- as.matrix(((dat-t(design%*%B.hat))^2))%*%rep(1/n.array,n.array)}else{var.pooled <- apply(dat-t(design%*%B.hat),1,var,na.rm=T)}
 
 	stand.mean <- t(grand.mean)%*%t(rep(1,n.array))
-	if(!is.null(design)){tmp <- design;tmp[,c(1:n.batch)] <- 0;stand.mean <- stand.mean+t(tmp%*%B.hat)}	
+	if(!is.null(design)){tmp <- design;tmp[,c(1:n.batch)] <- 0;stand.mean <- stand.mean+t(tmp%*%B.hat)}
 	s.data <- (dat-stand.mean)/(sqrt(var.pooled)%*%t(rep(1,n.array)))
 
 	##Get regression batch effect parameters
@@ -82,7 +82,7 @@ ComBat <- function(expression_xls, sample_info_file, type='txt', write=T, covari
 	a.prior <- apply(delta.hat, 1, aprior)
 	b.prior <- apply(delta.hat, 1, bprior)
 
-	
+
 	##Plot empirical and parametric priors
 
 	if (prior.plots & par.prior){
@@ -91,19 +91,19 @@ ComBat <- function(expression_xls, sample_info_file, type='txt', write=T, covari
 		plot(tmp,  type='l', main="Density Plot")
 		xx <- seq(min(tmp$x), max(tmp$x), length=100)
 		lines(xx,dnorm(xx,gamma.bar[1],sqrt(t2[1])), col=2)
-		qqnorm(gamma.hat[1,])	
-		qqline(gamma.hat[1,], col=2)	
-	
+		qqnorm(gamma.hat[1,])
+		qqline(gamma.hat[1,], col=2)
+
 		tmp <- density(delta.hat[1,])
 		invgam <- 1/rgamma(ncol(delta.hat),a.prior[1],b.prior[1])
 		tmp1 <- density(invgam)
 		plot(tmp,  typ='l', main="Density Plot", ylim=c(0,max(tmp$y,tmp1$y)))
 		lines(tmp1, col=2)
-		qqplot(delta.hat[1,], invgam, xlab="Sample Quantiles", ylab='Theoretical Quantiles')	
-		lines(c(0,max(invgam)),c(0,max(invgam)),col=2)	
+		qqplot(delta.hat[1,], invgam, xlab="Sample Quantiles", ylab='Theoretical Quantiles')
+		lines(c(0,max(invgam)),c(0,max(invgam)),col=2)
 		title('Q-Q Plot')
 	}
-	
+
 	##Find EB batch adjustments
 
 	gamma.star <- delta.star <- NULL
@@ -155,7 +155,7 @@ filter.absent <- function(x,pct){
 	present
 	}
 
-# Next two functions make the design matrix (X) from the sample info file 
+# Next two functions make the design matrix (X) from the sample info file
 build.design <- function(vec, des=NULL, start=2){
 	tmp <- matrix(0,length(vec),nlevels(vec)-start+1)
 	for (i in 1:ncol(tmp)){tmp[,i] <- vec==levels(vec)[i+start-1]}
@@ -233,7 +233,7 @@ int.eprior <- function(sdat,g.hat,d.hat){
 	r <- nrow(sdat)
 	for(i in 1:r){
 		g <- g.hat[-i]
-		d <- d.hat[-i]		
+		d <- d.hat[-i]
 		x <- sdat[i,!is.na(sdat[i,])]
 		n <- length(x)
 		j <- numeric(n)+1
@@ -248,8 +248,8 @@ int.eprior <- function(sdat,g.hat,d.hat){
 		}
 	adjust <- rbind(g.star,d.star)
 	rownames(adjust) <- c("g.star","d.star")
-	adjust	
-	} 
+	adjust
+	}
 
 #fits the L/S model in the presence of missing data values
 
@@ -259,4 +259,3 @@ Beta.NA = function(y,X){
 	B <- solve(t(des)%*%des)%*%t(des)%*%y1
 	B
 	}
-
