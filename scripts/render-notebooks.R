7845ecd05bf8d2e9ee8846f3c0c252bddba935d8
Candace Savonen <cansav09@gmail.com>
2020-08-05 16:18:55 -0400

    Make the script un-non-uni-monolithic

--- scripts/render-notebooks.R
+++ scripts/render-notebooks.R
@@ -3,19 +3,23 @@
 # Make rendered versions of .Rmd files with automagic citations with a specified
 # .bib file.
 
-# Find the project root
-root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
+# Example use:
 
-# List of files to render
-infiles <- c(
-  file.path(root_dir, "01-getting-started", "getting-started.Rmd")
-)
+# Rscript scripts/render-notesbooks.R \
+# -r 01-getting-started/getting-started.Rmd
+# -b references.bib
 
 # Load library:
 library(optparse)
 
 # Set up optparse options
 option_list <- list(
+  make_option(
+    opt_str = c("-r", "--rmd"), type = "character",
+    default = NULL,
+    help = "File path to an .Rmd file that should have the bib_file added",
+    metavar = "character"
+  ),
   make_option(
     opt_str = c("-b", "--bib_file"), type = "character",
     default = "references.bib", # Default is this file, but it can be changed
@@ -27,48 +31,43 @@ option_list <- list(
 # Parse options
 opt <- parse_args(OptionParser(option_list = option_list))
 
-# Make it normalized to root_dir
-normalized_bib_file <- normalizePath(opt$bib_file, mustWork = TRUE)
+# Make these file paths
+opt$bib_file <- file.path(opt$bib_file)
+opt$rmd <- file.path(opt$rmd)
 
-# Check that the file exists in the root directory
-if (!file.exists(normalized_bib_file)) {
-  message(normalized_bib_file)
+# Check that the file exists
+if (!file.exists(opt$rmd)) {
+  stop("Rmd file specified with --rmd is not found.")
+}
+
+# Check that the file exists
+if (!file.exists(opt$bib_file)) {
   stop("File specified for --bib_file option is not at the specified file path.")
 } else {
   bib_specify_line <- paste("bibliography:", normalized_bib_file)
 }
 
-# Make specialized function for adding a header specification
-add_header_line <- function(infile,
-                            outfile = NULL,
-                            header_line) {
+# Specify the output file
+outfile <-  stringr::str_replace(infile, ".Rmd", "tmp-header-changed.Rmd")
 
-  if (is.null(outfile)) {
-    outfile <-  file.path(dirname(infile), "tmp-header-changed.Rmd")
-  }
-  
-  # Read in as lines
-  lines <- readr::read_lines(infile)
+# Read in as lines
+lines <- readr::read_lines(infile)
 
-  # Find which lines are the beginning and end of the header chunk
-  header_range <- which(lines == "---")
+# Find which lines are the beginning and end of the header chunk
+header_range <- which(lines == "---")
 
-  # Add the bibliography specification line at the beginning of the chunk
-  new_lines <- append(lines, header_line, header_range[1])
+# Add the bibliography specification line at the beginning of the chunk
+new_lines <- append(lines, header_line, header_range[1])
 
-  # Write to an outfile
-  readr::write_lines(new_lines, outfile)
+# Write to an outfile
+readr::write_lines(new_lines, outfile)
 
-  # Render the header added notebook
-  rmarkdown::render(outfile,
-    output_format = rmarkdown::html_document(),
-    # Save to original html output file name
-    output_file = stringr::str_replace(infile, ".Rmd", ".nb.html")
-  )
+# Render the header added notebook
+rmarkdown::render(outfile,
+  output_format = rmarkdown::html_document(),
+  # Save to original html output file name
+  output_file = stringr::str_replace(infile, ".Rmd", ".nb.html")
+)
 
-  # Remove the temporary header change .Rmd output file
-  file.remove(outfile)
-}
-
-# Render all the notebooks but with added header line
-purrr::map(infiles, add_header_line, header_line = normalized_bib_file)
+# Remove the temporary header change .Rmd output file
+file.remove(outfile)
