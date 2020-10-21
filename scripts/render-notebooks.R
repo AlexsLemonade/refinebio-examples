4ab1d802c59d3fc0d2963f5c5ad0857e119209c7
Candace Savonen <cansav09@gmail.com>
2020-10-21 16:18:20 -0400

    Add google analytics to renderings (#314)
    * Try adding google analytics
    
    * Add to header using includes
    
    * temporary file snuck in there
    
    * Restore master version so they aren't in the review
    
    * Let's call an html file and html file
    
    * Docker dep fix: Add lib package 40 thing that clusterprofiler needs (#316)
    
    * Add lib package 40 thing that clusterprofiler needs
    
    * Try adding options(warn = 2)
    
    * Test if options(warn =2) means it breaks like it should
    
    * Revert "Test if options(warn =2) means it breaks like it should"
    
    This reverts commit d9f688f68448ef69fe4c1caa48af23051cd7f4e3.

--- scripts/render-notebooks.R
+++ scripts/render-notebooks.R
@@ -107,13 +107,17 @@ new_lines <- append(lines, header_line, header_range[1])
 # Write to an tmp file
 readr::write_lines(new_lines, tmp_file)
 
+# Declare path to google analytics bit
+google_analytics_file <- normalizePath(file.path("components", "google-analytics.html"))
+
 # Render the header added notebook
 rmarkdown::render(tmp_file,
   output_format = rmarkdown::html_document(
     toc = TRUE, toc_depth = 2,
     toc_float = TRUE, number_sections = TRUE,
     df_print = "paged",
-    highlight = "haddock"
+    highlight = "haddock",
+    includes = rmarkdown::includes(in_header = google_analytics_file)
   ),
   # Save to original html output file name
   output_file = output_file
