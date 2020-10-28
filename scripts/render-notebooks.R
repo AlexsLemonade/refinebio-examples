abfddf38f59eba8dea676c91da4a6d922277a5b3
Chante Bethell <chantebethell@gmail.com>
2020-10-28 13:28:44 -0400

    Revert "Making staging changes live (#329)"
    This reverts commit 052286cb4bf60b6dd7703593ce697d15e861f8cc.

--- scripts/render-notebooks.R
+++ scripts/render-notebooks.R
@@ -107,17 +107,13 @@ new_lines <- append(lines, header_line, header_range[1])
 # Write to an tmp file
 readr::write_lines(new_lines, tmp_file)
 
-# Declare path to google analytics bit
-google_analytics_file <- normalizePath(file.path("components", "google-analytics.html"))
-
 # Render the header added notebook
 rmarkdown::render(tmp_file,
   output_format = rmarkdown::html_document(
     toc = TRUE, toc_depth = 2,
     toc_float = TRUE, number_sections = TRUE,
     df_print = "paged",
-    css = normalizePath(file.path("components", "styles.css")),
-    includes = rmarkdown::includes(in_header = google_analytics_file)
+    css = normalizePath(file.path("components", "styles.css"))
   ),
   # Save to original html output file name
   output_file = output_file
