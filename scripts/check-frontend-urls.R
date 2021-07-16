fa2ec6a8a440fdf959b1a88abbe79718fe557c14
David Mejia <davidsmejia@gmail.com>
2021-07-16 16:41:50 -0400

    unique the expected files

--- scripts/check-frontend-urls.R
+++ scripts/check-frontend-urls.R
@@ -23,7 +23,7 @@ expected_urls <- fromJSON(txt=opt$links)
 expected_files <- stringr::word(expected_urls, 2, sep = '/refinebio-examples/')
 expected_id_paths <- stringr::str_subset(expected_files, '#')
 # remove anchors
-expected_files <- stringr::word(expected_files, 1, sep = '#')
+expected_files <- unique(stringr::word(expected_files, 1, sep = '#'))
 
 # get a list of all html files in the project
 existing_files <- list.files(pattern = 'html$', recursive = TRUE)
@@ -33,7 +33,9 @@ missing_files <- expected_files[!expected_files %in% existing_files]
 
 # check for missing ids after we know the file isnt missing
 # if a file and id is missing it will first complain that the file is missing
-expected_id_paths <- expected_id_paths[!grepl(paste(missing_files, collapse="|"), expected_id_paths)]
+if (any(missing_files)) {
+  expected_id_paths <- expected_id_paths[!grepl(paste(missing_files, collapse="|"), expected_id_paths)]
+}
 # find missing ids
 missing_ids = c()
 
